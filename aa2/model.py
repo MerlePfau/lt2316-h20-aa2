3a15866973a493deae545de6776113b488f3a9ed
Merle Pfau <guspfame@GU.GU.SE@mltgpu.flov.gu.se>
2020-10-08 15:03:24 +0200

    model update

--- aa2/model.py
+++ aa2/model.py
@@ -15,32 +15,31 @@ class LSTM_fixed_len(nn.Module) :
         self.dropout = nn.Dropout(0.2)
     
     
-    def forward(self, x):
-        # Initialize hidden state with zeros
-        h0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).requires_grad_()
-        h0 = h0.to(self.device)
+#     def forward(self, x):
+#         # Initialize hidden state with zeros
+#         h0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).requires_grad_()
+#         h0 = h0.to(self.device)
 
-        # Initialize cell state
-        c0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).requires_grad_()
-        c0 = c0.to(self.device)
+#         # Initialize cell state
+#         c0 = torch.zeros(self.n_layers, x.size(0), self.hidden_dim).requires_grad_()
+#         c0 = c0.to(self.device)
 
-        # 28 time steps
-        # We need to detach as we are doing truncated backpropagation through time (BPTT)
-        # If we don't, we'll backprop all the way to the start even after going through another batch
-        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+#         # 28 time steps
+#         # We need to detach as we are doing truncated backpropagation through time (BPTT)
+#         # If we don't, we'll backprop all the way to the start even after going through another batch
+#         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
 
-        # Index hidden state of last time step
-        # out.size() --> 100, 28, 100
-        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
-        out = self.linear(out[:, -1, :]) 
-        # out.size() --> 100, 10
-        return out
+#         # Index hidden state of last time step
+#         # out.size() --> 100, 28, 100
+#         # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+#         out = self.linear(out[:, -1, :]) 
+#         # out.size() --> 100, 10
+#         return out
     
-#     def forward(self, batch, hidden):
-#         batch_size = batch.size(0)
-#         lstm_out, (h, c) = self.lstm(batch, hidden)
-#         lstm_out = self.dropout(lstm_out)
-      
-#         output = self.linear(h[-1])
-#         return output, hidden
+    def forward(self, batch):
+        lstm_out, (h, c) = self.lstm(batch)
+        lstm_out = self.dropout(lstm_out)
+     
+        output = self.linear(h[-1])
+        return output, hidden
     
